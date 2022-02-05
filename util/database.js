615da2493019f9ff94ae7f7aa9bab7839f9305e3
Abhi Kulshrestha <40908684+AbhiKul10@users.noreply.github.com>
2022-02-05 18:39:16 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -8,7 +8,7 @@ const pool = mysql.createPool({
   password: process.env.PASSWORD,
 });
 
-let createTable = `CREATE TABLE IF NOT EXISTS usersss (
+let createTable = `CREATE TABLE IF NOT EXISTS users (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   email VARCHAR(45) NOT NULL,
   password VARCHAR(255) NOT NULL,
