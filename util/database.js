73e34696af11e2f297adc57b236873503dd83d30
Abhi Kulshrestha <40908684+AbhiKul10@users.noreply.github.com>
2022-02-05 14:53:08 +0530

    updated Database, added process.env

--- util/database.js
+++ util/database.js
@@ -1,10 +1,24 @@
 const mysql = require("mysql2");
+require('dotenv').config()
 
 const pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: "root",
-  database: "ask-score",
-  password: "Abhi1234",
+  host: process.env.APP_HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+});
+
+pool.on('connection', function (connection) {
+  console.log('DB Connection established');
+  
+
+  connection.on('error', function (err) {
+    console.error(new Date(), 'MySQL error', err.code);
+  });
+  connection.on('close', function (err) {
+    console.error(new Date(), 'MySQL close', err);
+  });
+
 });
 
 module.exports = pool.promise();
